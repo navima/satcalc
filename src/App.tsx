88ecec1eff1c803a0ace404d9f4bc3d04edb7cca
navima <draco.109@gmail.com>
2023-03-25 11:30:24 +0100

    Add basic interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import CalculatorService from './service';
-import { ItemRate } from './model';
+import { Graph, ItemRate } from './model';
 
 function App() {
-	const calculatorService = new CalculatorService();
-	const resultGraph = calculatorService.calculate([], [new ItemRate(calculatorService.items.get('reinforcedIronPlate')!, 5)]);
+	const [calculatorService, setCalculatorService] = useState(() => new CalculatorService());
+
+	const [item, setItem] = React.useState(calculatorService.items.get('reinforcedIronPlate')!);
+	const [amount, setAmount] = React.useState(60);
+	const [itemInputValue, setItemInputValue] = React.useState(item.name);
+	const [resultGraph, setResultGraph] = useState<Graph>();
+
+	useEffect(() => {
+		setResultGraph(calculatorService.calculate([], [new ItemRate(item, amount)]));
+	}, [item, amount]);
+
 	return (
 		<div>
-			Hi
+			<input list='items' value={itemInputValue} onChange={(e) => {
+				setItemInputValue(e.target.value);
+				if (calculatorService.items.has(e.target.value)) {
+					setItem(calculatorService.items.get(e.target.value)!);
+				}
+			}} />
+			<input type='number' value={amount} onChange={(e) => setAmount(+e.target.value)} />
+			<datalist id='items'>
+				{Array.from(calculatorService.items.entries(), entry => (
+					<option key={entry[0]} value={entry[0]}>{entry[1].name}</option>
+				))}
+			</datalist>
 		</div>
 	);
 }
